02cc8309d779c5c10bfad9dab18038ccd4b47de6
Carl Boettiger <cboettig@gmail.com>
2024-02-16 06:20:02 +0000

    version 0.1.5

--- inst/doc/vignette.R
+++ inst/doc/vignette.R
@@ -1,38 +1,38 @@
-## ----include=FALSE-------------------------------------------------------
+## ----include=FALSE------------------------------------------------------------
 knitr::opts_chunk$set(eval = FALSE)
 
-## ----install, eval=FALSE-------------------------------------------------
+## ----install, eval=FALSE------------------------------------------------------
 #  install.packages('treebase')
 
-## ----loadpkg-------------------------------------------------------------
+## ----loadpkg------------------------------------------------------------------
 #  library(treebase)
 
-## ----search1, eval=FALSE, message=FALSE, warning=FALSE-------------------
+## ----search1, eval=FALSE, message=FALSE, warning=FALSE------------------------
 #  both <- search_treebase("Ronquist or Hulesenbeck", by=c("author", "author"))
 #  dolphins <- search_treebase('"Delphinus"', by="taxon", max_trees=5)
 #  studies <- search_treebase("2377", by="id.study")
 
-## ----search2, message=FALSE, warning=FALSE-------------------------------
+## ----search2, message=FALSE, warning=FALSE------------------------------------
 #  Near <- search_treebase("Near", "author", branch_lengths=TRUE, max_trees=3)
 #  Near[1]
 
-## ----metadata, eval=FALSE, message=FALSE, warning=FALSE------------------
+## ----metadata, eval=FALSE, message=FALSE, warning=FALSE-----------------------
 #  all <- download_metadata("", by="all")
 #  dates <- sapply(all, function(x) as.numeric(x$date))
 #  library(ggplot2)
 #  qplot(dates, main="Treebase growth", xlab="Year", binwidth=.5)
 
-## ----eval=FALSE, message=FALSE, warning=FALSE----------------------------
+## ----eval=FALSE, message=FALSE, warning=FALSE---------------------------------
 #  nature <- sapply(all, function(x) length(grep("Nature", x$publisher))>0)
 #  science <- sapply(all, function(x) length(grep("^Science$", x$publisher))>0)
 #  sum(nature)
 #  sum(science)
 
-## ----message=FALSE, warning=FALSE----------------------------------------
+## ----message=FALSE, warning=FALSE---------------------------------------------
 #  search_treebase("Derryberry", "author")[[1]] -> tree
 #  plot(tree)
 
-## ----message=FALSE, warning=FALSE, eval = FALSE--------------------------
+## ----message=FALSE, warning=FALSE, eval = FALSE-------------------------------
 #  library(laser)
 #  tt <- branching.times(tree)
 #  models <-  list(pb = pureBirth(tt),
@@ -51,7 +51,7 @@ knitr::opts_chunk$set(eval = FALSE)
 #  
 #  
 
-## ----eval=FALSE, message=FALSE, warning=FALSE----------------------------
+## ----eval=FALSE, message=FALSE, warning=FALSE---------------------------------
 #  timetree <- function(tree){
 #      check.na <- try(sum(is.na(tree$edge.length))>0)
 #      if(is(check.na, "try-error") | check.na)
@@ -72,7 +72,7 @@ knitr::opts_chunk$set(eval = FALSE)
 #    as.logical(try(m2$aic < m1$aic))
 #  }
 
-## ----eval=FALSE, message=FALSE, warning=FALSE----------------------------
+## ----eval=FALSE, message=FALSE, warning=FALSE---------------------------------
 #  all <- search_treebase("Consensus", "type.tree", branch_lengths=TRUE)
 #  tt <- drop_errors(sapply(all, timetree))
 #  is_yule <- sapply(tt, pick_branching_model)
